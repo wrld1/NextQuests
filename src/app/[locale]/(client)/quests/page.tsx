ee62a8508319289752e4df2839d50cefeae3e294
Kyrylo Lolenko <kir260302@gmail.com>
2024-05-09 01:42:56 +0300

    Comment out getQuests on quests page for deploy

--- src/app/[locale]/(client)/quests/page.tsx
+++ src/app/[locale]/(client)/quests/page.tsx
@@ -1,18 +1,9 @@
-import { categories } from "@/constants/categories";
 import React, { Suspense } from "react";
 import QuestCardList from "./_components/QuestCardList";
 
 import SkeletonList from "./_components/SkeletonList";
 
-export async function generateStaticParams() {
-  return categories.map((category) => ({ category: category.type }));
-}
-
-export default async function QuestsPage({
-  params,
-}: {
-  params: { category: string };
-}) {
+export default async function QuestsPage() {
   return (
     <div className="flex gap-2">
       <Suspense fallback={<SkeletonList />}>
