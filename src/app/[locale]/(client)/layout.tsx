c748f75f7d18972c88ff3de11e7d9a3c49650c2d
Kyrylo Lolenko <kir260302@gmail.com>
2024-05-09 18:04:04 +0300

    change close AuthModal button from Link to router.back()

--- src/app/[locale]/(client)/layout.tsx
+++ src/app/[locale]/(client)/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Raleway } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
--- src/components/ui/AuthModal/AuthModal.tsx
+++ src/components/ui/AuthModal/AuthModal.tsx
@@ -1,4 +1,6 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
 import { X } from "react-feather";
 
 type AuthModalProps = {
@@ -6,13 +8,21 @@ type AuthModalProps = {
 };
 
 function AuthModal({ children }: AuthModalProps) {
+  const router = useRouter();
+
   return (
     <div className="fixed z-50 top-0 flex justify-center items-center min-w-full min-h-full bg-darkTransparentBackground bg-opacity-80">
       <div className="bg-formsBackground p-8 shadow-lg rounded-md z-50 ">
         <div className="text-center">
-          <Link href="/" className="flex justify-end">
-            <X className="text-white" />
-          </Link>
+          <div className="flex justify-end">
+            <button
+              onClick={() => {
+                router.back();
+              }}
+            >
+              <X className="text-white" />
+            </button>
+          </div>
           <div className="">{children}</div>
         </div>
       </div>
