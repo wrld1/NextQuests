e7dc7b433ad07a45bb86bb495f416be724d657f3
Kyrylo Lolenko <kir260302@gmail.com>
2024-05-22 21:04:55 +0300

    Make getQuests route dynamic

--- src/app/api/quests/route.ts
+++ src/app/api/quests/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import db from "@/modules/db";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
   const quests = await db.quest.findMany();
 
