a3e5fcc321ca81ad514dba67ceb750aee7a57984
Kyrylo Lolenko <kir260302@gmail.com>
2024-05-22 20:57:49 +0300

    Make quests page dynamic

--- src/app/[locale]/quests/page.tsx
+++ src/app/[locale]/quests/page.tsx
@@ -2,6 +2,8 @@ import React, { Suspense } from "react";
 import QuestCardList from "./_components/QuestCardList";
 import SkeletonList from "./_components/SkeletonList";
 
+export const dynamic = "force-dynamic";
+
 export default async function QuestsPage() {
   return (
     <div className="flex gap-2">
